5e4fc07dd8b44622bf3dddb02660fa5334bc4953
wagnercsfilho <wagnercsfilho@hotmail.com>
2015-08-31 21:36:18 +0000

    docs page site

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'site'));
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/site'));
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+  res.render('index');
+});
+
+app.get('/getting-started.html', function (req, res) {
+  res.render('getting-started');
+});
+
+app.get('/docs.html', function (req, res) {
+  res.render('docs');
+});
+
+app.get('/blog.html', function (req, res) {
+  res.render('blog');
+});
+
+app.get('/examples.html', function (req, res) {
+  res.render('examples');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
